902d0ce6e557be0cc79dc94f2367c95f79da935f
Mason M <mason@bandoti.com>
2023-09-12 15:20:21 -0300

    Fix lsmfs unwrapped startup path

--- app/lsmfs/main.tcl
+++ app/lsmfs/main.tcl
@@ -1,6 +1,15 @@
 # Copyright (c) 2018 ActiveState Software Inc.
 # Released under the BSD-3 license. See LICENSE file for details.
 #
+
+set self [file dirname [file dirname [file dirname [file normalize [info script]]]]]
+
 package require starkit
+if {"unwrapped" eq [starkit::startup]} {
+    lappend auto_path [file join $self lib]
+    # External standard actions
+    source [file join $self app main_std.tcl]
+    puts stderr unwrapped\n[join $auto_path \n\t]
+}
 starkit::startup
 package require app-listmfs
