2b5ebd4a5327ec4147fc0b00a8e1b342054056fe
Mason M <mason@bandoti.com>
2024-02-02 13:25:24 -0400

    Fix issues generating pkg's to copy

--- package_location.tcl
+++ package_location.tcl
@@ -14,13 +14,17 @@ set re [string map [list %DIR% $distroLibDir] {(?:source|load) (%DIR%[^\s]+)}]
 
 foreach p $packages {
     if {! [catch {package ifneeded $p [package require $p]} res ropt]} {
-	set path [lindex [regexp -inline -- $re $res] 1]
-	if {"$path" ne ""} {
-	    set pkgDirName [lindex [file split [string map [list $distroLibDir/ {}] $path]] 0]
-	    set pkgDir "${distroLibDir}/${pkgDirName}"
-	    puts -nonewline "$p=$pkgDir "
+		set staticExtension [regexp {load\s+\{\}} $res]
+		if {$staticExtension} {
+			set pkgDir NA
+
+		} else {
+			set path [lindex [regexp -inline -- $re $res] 1]
+			set pkgDirName [lindex [file split [string map [list $distroLibDir/ {}] $path]] 0]
+			set pkgDir "${distroLibDir}/${pkgDirName}"
+		}
+		puts -nonewline "$p=$pkgDir "
 	}
-    }
 }
 
 catch {destroy .}
